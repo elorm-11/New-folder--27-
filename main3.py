4dc7a9a18e6f02537b5f1ee33b057a9b0b1b1f03
elorm-11 <elormkovey@gmail.com>
2024-11-27 19:59:41 +0000

    4 commit

---
+++ main3.py
@@ -0,0 +1,16 @@
+student = 'John Doe', 27, 'Python v3', 1234567890
+
+print (student)
+('John Doe', 27, 'Python v3', 1234567890)
+
+name, age, course, phone = student
+
+print (name)
+print (age)
+print (course)
+print (phone)
+
+John Doe
+27
+Python v3
+1234567890
